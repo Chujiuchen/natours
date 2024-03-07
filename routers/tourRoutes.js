a317aa43c41a833edeadd9b66f5ec47e99e2576b
chujiu <letchujiufly@outlook.com>
2024-03-07 09:16:22 +0800

    Extract tour id

--- routers/tourRoutes.js
+++ routers/tourRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const tourController = require('../controller/tourController')
 const router = express.Router();
 
+router.param('id',tourController.checkId);
+
 router.route('/').get(tourController.getAllTours).post(tourController.createTour);
 router.route('/:id').patch(tourController.updateTour).delete(tourController.deleteTour).get(tourController.getTour);
 
