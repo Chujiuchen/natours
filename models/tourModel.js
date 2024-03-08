5e5344383749cf20136515f4ce3e17317446e631
chujiu <letchujiufly@outlook.com>
2024-03-08 17:13:55 +0800

    Create virtual function

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,56 +1,65 @@
 const mongoose = require('mongoose');
 
 const tourSchema = mongoose.Schema({
-	name: {
-		type: String,
-		required: [true, 'A tour must hava a name!'],
-		unique: true
-	},
-	duration: {
-		type: Number,
-		required: [true, 'A tour must have duration!']
-	},
-	maxGroupSize: {
-		type: Number,
-		required: [true, 'A tour must have group size!']
-	},
-	difficulty: {
-		type: String,
-		required: [true, 'A tour must have difficulty!']
-	},
-	ratingsAverage: {
-		type: Number,
-		default: 4.5
-	},
-	ratingsQuantity: {
-		type: Number,
-		default: 0
-	},
-	price: {
-		type: Number,
-		required: [true, 'A tour must have a price!']
-	},
-	priceDiscount: Number,
-	summary: {
-		type: String,
-		trim: true
-	},
-	description: {
-		type: String,
-		trim: true
-	},
-	imageCover: {
-		type: String,
-		required: [true, 'A tour must have a img!']
-	},
-	images: [String],
-	createAt: {
-		type: Date,
-		default: Date.now(),
-		select: false
-	},
-	startDates: [Date]
+		name: {
+			type: String,
+			required: [true, 'A tour must hava a name!'],
+			unique: true
+		},
+		duration: {
+			type: Number,
+			required: [true, 'A tour must have duration!']
+		},
+		maxGroupSize: {
+			type: Number,
+			required: [true, 'A tour must have group size!']
+		},
+		difficulty: {
+			type: String,
+			required: [true, 'A tour must have difficulty!']
+		},
+		ratingsAverage: {
+			type: Number,
+			default: 4.5
+		},
+		ratingsQuantity: {
+			type: Number,
+			default: 0
+		},
+		price: {
+			type: Number,
+			required: [true, 'A tour must have a price!']
+		},
+		priceDiscount: Number,
+		summary: {
+			type: String,
+			trim: true
+		},
+		description: {
+			type: String,
+			trim: true
+		},
+		imageCover: {
+			type: String,
+			required: [true, 'A tour must have a img!']
+		},
+		images: [String],
+		createAt: {
+			type: Date,
+			default: Date.now(),
+			select: false
+		},
+		startDates: [Date]
+	}, {
+		toJSON: { virtuals: true },
+		toObject: { virtuals: true }
+	}
+);
+
+tourSchema.virtual('durationWeeks').get(function() {
+	return this.duration / 7;
 });
+
 
 const Tour = mongoose.model('Tour', tourSchema);
 
