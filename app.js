78ca0c67d2bd6bbb0fe2846a71e35ed304589627
chujiu <letchujiufly@outlook.com>
2024-03-09 13:58:13 +0800

    Url error message

--- app.js
+++ app.js
@@ -15,4 +15,12 @@ app.use(express.static(`${__dirname}/public`));
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+//不是指定的路由 就提示404
+app.all('*',(req, res,next)=>{
+	res.status(404).json({
+		status:'fail',
+		message:`Can not fond ${req.originalUrl} on this server!}`
+	})
+});
+
 module.exports = app;
