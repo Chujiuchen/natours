f642a914e283e66176480ea2c22bd6fb6bc825bf
chujiu <letchujiufly@outlook.com>
2024-03-09 17:11:44 +0800

    Create error display in dev and prod environment

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,10 +1,39 @@
 //自定义的错误处理中间键
+const sendErrorDev = (err, res) => {
+	res.status(err.statusCode).json({
+		status: err.status,
+		error: err,
+		message: err.message,
+		stack: err.stack
+	});
+};
+
+const sendErrorProd = (err, res) => {
+	if (err.isOperational) {
+		res.status(err.statusCode).json({
+			status: err.status,
+			message: err.message
+		});
+	} else {
+		console.error('Error:');
+		res.status(500).json({
+			status: 'error',
+			message: 'Something was wrong!'
+		});
+	}
+
+};
+
 module.exports = (err, req, res, next) => {
 	err.statusCode = err.statusCode || 500;
 	err.status = err.status || 'error';
-	// console.log(2)
-	res.status(err.statusCode).json({
-		status: err.status,
-		message: err.message
-	});
+	console.log(process.env.NODE_ENV);
+	console.log(process.env.NODE_ENV === 'development');
+	if (process.env.NODE_ENV === 'development') {
+		sendErrorDev(err, res);
+	} else if (process.env.NODE_ENV === 'production') {
+		// console.log(process.env.NODE_ENV);
+		sendErrorProd(err, res);
+	}
+
 };
