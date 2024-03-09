ab3510ac1ca93c3c16abbe6dfe1281c73e76c782
chujiu <letchujiufly@outlook.com>
2024-03-09 15:15:26 +0800

    Create error middleware

--- app.js
+++ app.js
@@ -17,10 +17,25 @@ app.use('/api/v1/users', userRouter);
 
 //不是指定的路由 就提示404
 app.all('*',(req, res,next)=>{
-	res.status(404).json({
-		status:'fail',
-		message:`Can not fond ${req.originalUrl} on this server!}`
-	})
+	// res.status(404).json({
+	// 	status:'fail',
+	// 	message:`Can not fond ${req.originalUrl} on this server!}`
+	// })
+	const err = new Error(`Can not fond ${req.originalUrl} on this server!}`)
+	err.status = 'fail';
+	err.statusCode = 404;
+	console.log(1)
+	next(err);
 });
+
+app.use((err,req,res,next)=>{
+	err.statusCode = err.statusCode || 500;
+	err.status = err.status ||'error';
+	console.log(2)
+	res.status(err.statusCode).json({
+		status:err.status,
+		message:err.message
+	})
+})
 
 module.exports = app;
