9fa3fc65797f7336f0c255d7bef1ac512451a94f
chujiu <letchujiufly@outlook.com>
2024-03-11 09:59:25 +0800

    unhandledRejection and uncaughtException

--- server.js
+++ server.js
@@ -35,6 +35,23 @@ mongoose.connect(DB, {
 // 	console.log('Error :', err);
 // });
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
+});
+
+//server 启动错误提示
+process.on('unhandledRejection', err => {
+	console.log(err.name, err.message);
+	console.log('Unhandled Rejection: PLEASE CHECK CODE!');
+	server.close(() => {
+		process.exit(1);
+	});
+});
+
+process.on('uncaughtException', err => {
+	console.log(err.name, err.message);
+	console.log('Uncaught Exception: PLEASE CHECK CODE!');
+	server.close(() => {
+		process.exit(1);
+	});
 });
