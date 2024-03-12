826d98ff6b3e41040b770d07092eac697dcca90e
chujiu <letchujiufly@outlook.com>
2024-03-12 16:10:06 +0800

    Data sanitization via express-mongo-sanitize and xss-clean

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controller/errorController');
@@ -28,6 +30,11 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json({
 	limit: '10kb'
 }));
+
+//NoSQL query injection 防止"email":{"$gt":""} 直接就能登录
+app.use(mongoSanitize());
+//XSS 这个将去除如何用户输入的html代码
+app.use(xss());
 //express能读取到public文件中的所有文件
 app.use(express.static(`${__dirname}/public`));
 
