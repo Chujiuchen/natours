35af25f532398aee22b1b2ca943d1bde7ca72a69
chujiu <letchujiufly@outlook.com>
2024-03-30 17:40:33 +0800

    Update user password with our api

--- public/js/index.js
+++ public/js/index.js
@@ -6,6 +6,7 @@ const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const updateUser = document.querySelector('.form-user-data');
+const updateUserPassword = document.querySelector('.form-user-password');
 //进行数据判断
 if (mapBox) {
 	const locations = JSON.parse(mapBox.dataset.locations);
@@ -29,6 +30,21 @@ if (updateUser) {
 		e.preventDefault();//阻止默认提交
 		const name = document.getElementById('name').value;//获取输入框的name值
 		const email = document.getElementById('email').value;//获取输入框的email值
-		updateSettings(name, email);//调用更新函数
+		updateSettings({ name, email }, 'data');//调用更新函数
+	});
+}
+if (updateUserPassword) {
+	updateUserPassword.addEventListener('submit', async e => {
+		e.preventDefault();
+		document.querySelector('.btn-password-change').textContent = 'Updating...';
+		const passwordCurrent = document.getElementById('password-current').value;
+		const password = document.getElementById('password').value;
+		const passwordConfirm = document.getElementById('password-confirm').value;
+	  await	updateSettings({ passwordCurrent, password, passwordConfirm }, 'password');
+
+		document.querySelector('.btn-password-change').textContent = 'Save password';
+		document.getElementById('password-current').value = '';
+		document.getElementById('password').value = '';
+		document.getElementById('password-confirm').value = '';
 	});
 }
--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -1,23 +1,23 @@
 import axios from 'axios';
 import { showAlert } from './alerts';
 
-export const updateSettings =async (name, email) => {
+export const updateSettings = async (data, type) => {
 	try {
+		const url = type === 'password' ? 'http://127.0.0.1:3000/api/v1/users/updateMyPassword' : 'http://127.0.0.1:3000/api/v1/users/updateMe';
 		// Send a PATCH request to update user settings
 		const res = await axios({
 			method: 'PATCH',
-			url: 'http://127.0.0.1:3000/api/v1/users/updateMe',
-			data: {
-				name,
-				email
-			}
+			url,
+			data
 		});
 		// If the response is successful, show ia success alert and redirect to the user's profile page
 		if (res.data.status === 'success') {
-			showAlert('success', 'Update Settings Successfully!');
+			const message = type === 'password'? 'Password updated successfully!Please login again' : 'User settings updated successfully';
+			showAlert('success', message);
+			const type = 'password'? '/login' : '/me';
 			window.setTimeout(() => {
-				window.location.href = '/me';
-			}, 3000);
+				window.location.href = type;
+			}, 2000);
 		}
 	} catch (error) {
 		showAlert('error', error.response.data.message);
