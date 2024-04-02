a06c55794a6b4d10600caa09bd334da85b7584f6
chujiu <letchujiufly@outlook.com>
2024-04-02 19:46:17 +0800

    Config multer for upload user photo

--- controller/userController.js
+++ controller/userController.js
@@ -1,12 +1,40 @@
+const multer = require('multer');
 const User = require('../models/userModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('./../utils/catchAsync');
 const factory = require('./handlerFactory');
 
+// 设置 multer 存储配置，指定上传文件的目标路径和文件名
+const multerStorage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, 'public/img/users'); // 将上传文件存储到指定目录
+	},
+	filename: (req, file, cb) => {
+		const ext = file.mimetype.split('/')[1]; // 获取文件扩展名
+		cb(null, `user-${req.user.id}-${Date.now()}.${ext}`); // 设置文件名格式
+	},
+});
+
+// 设置 multer 文件过滤器，检查文件类型是否为图像
+const multerFilter = (req, file, cb) => {
+	if (file.mimetype.startsWith('image')) {
+		cb(null, true); // 符合条件，允许上传
+	} else {
+		cb(new AppError('Please upload an image file', 400), false); // 不符合条件，返回错误信息
+	}
+};
+
+// 创建 upload 对象，应用存储配置和文件过滤器
+const upload = multer({
+	storage: multerStorage,
+	fileFilter: multerFilter,
+});
+
+exports.uploadUserPhoto = upload.single('photo');
 
 const filterObj = (obj, ...allowedFields) => {
 	const newObj = {};
-	Object.keys(obj).forEach(el => {
+	Object.keys(obj).forEach((el) => {
 		if (allowedFields.includes(el)) {
 			newObj[el] = obj[el];
 		}
@@ -23,9 +51,16 @@ exports.getMe = catchAsync(async (req, res, next) => {
 //更新用户的信息
 exports.updataMe = catchAsync(async (req, res, next) => {
 	// console.log(111)
+	console.log(req.file);
+	console.log(req.body);
 	//安全判断 如果包含密码直接弹个错误
 	if (req.body.password || req.body.passwordConfirm) {
-		return next(new AppError('This route is not for password update.Please use /updateMyPassword', 400));
+		return next(
+			new AppError(
+				'This route is not for password update.Please use /updateMyPassword',
+				400
+			)
+		);
 	}
 
 	//验证只允许更新的字段
@@ -34,14 +69,14 @@ exports.updataMe = catchAsync(async (req, res, next) => {
 	//更新用户数据 避免创建的验证信息 用findByIdAndUpdate(id,运行更新的字段,设置)
 	const updateUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
 		new: true,
-		runValidators: true
+		runValidators: true,
 	});
 
 	res.status(200).json({
 		status: 'success',
 		data: {
-			user: updateUser
-		}
+			user: updateUser,
+		},
 	});
 });
 
@@ -50,7 +85,7 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
 	await User.findByIdAndUpdate(req.user.id, { active: false });
 	res.status(204).json({
 		status: 'success',
-		data: null
+		data: null,
 	});
 });
 
--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+
 const userController = require('../controller/userController');
 const authController = require('../controller/authController');
 
@@ -13,13 +14,22 @@ router.route('/resetPassword/:token').patch(authController.resetPassword);
 //保护后面所有的路由都需要登录验证
 router.use(authController.protect);
 router.route('/updateMyPassword').patch(authController.updatePassword);
-router.route('/me').get(userController.getMe, userController.getUser);//通过getMe函数获取当前用户信息 然后在getUser函数中获取详细信息
-router.route('/updateMe').patch(userController.updataMe);
+router.route('/me').get(userController.getMe, userController.getUser); //通过getMe函数获取当前用户信息 然后在getUser函数中获取详细信息
+router
+	.route('/updateMe')
+	.patch(userController.uploadUserPhoto, userController.updataMe);
 router.route('/deleteMe').delete(userController.deleteMe);
 
 //admin才能访问的路由
 router.use(authController.restrictTo('admin'));
-router.route('/').get(userController.getAllUsers).post(userController.createUser);
-router.route('/:id').patch(userController.updateUser).delete(userController.deleteUser).get(userController.getUser);
+router
+	.route('/')
+	.get(userController.getAllUsers)
+	.post(userController.createUser);
+router
+	.route('/:id')
+	.patch(userController.updateUser)
+	.delete(userController.deleteUser)
+	.get(userController.getUser);
 
 module.exports = router;
